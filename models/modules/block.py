5969ef495895574f4a32d3182059cb08f39bc0df
Bin4writing <is.zhang.yueyou@gmail.com>
2019-03-01 03:08:44 +0800

    final3

--- models/modules/block.py
+++ models/modules/block.py
@@ -8,46 +8,16 @@ import torch.nn as nn
 
 
 def act(act_type, inplace=True, neg_slope=0.2, n_prelu=1):
-    # helper selecting activation
-    # neg_slope: for leakyrelu and init of prelu
-    # n_prelu: for p_relu num_parameters
-    act_type = act_type.lower()
-    if act_type == 'relu':
-        layer = nn.ReLU(inplace)
-    elif act_type == 'leakyrelu':
-        layer = nn.LeakyReLU(neg_slope, inplace)
-    elif act_type == 'prelu':
-        layer = nn.PReLU(num_parameters=n_prelu, init=neg_slope)
-    else:
-        raise NotImplementedError('activation layer [{:s}] is not found'.format(act_type))
-    return layer
-
+    if act_type=='relu':
+        return nn.ReLU(inplace)
+    return nn.LeakyReLU(neg_slope, inplace)
 
 def norm(norm_type, nc):
-    # helper selecting normalization layer
-    norm_type = norm_type.lower()
-    if norm_type == 'batch':
-        layer = nn.BatchNorm2d(nc, affine=True)
-    elif norm_type == 'instance':
-        layer = nn.InstanceNorm2d(nc, affine=False)
-    else:
-        raise NotImplementedError('normalization layer [{:s}] is not found'.format(norm_type))
-    return layer
+    return nn.BatchNorm2d(nc, affine=True)
 
 
 def pad(pad_type, padding):
-    # helper selecting padding layer
-    # if padding is 'zero', do by conv layers
-    pad_type = pad_type.lower()
-    if padding == 0:
-        return None
-    if pad_type == 'reflect':
-        layer = nn.ReflectionPad2d(padding)
-    elif pad_type == 'replicate':
-        layer = nn.ReplicationPad2d(padding)
-    else:
-        raise NotImplementedError('padding layer [{:s}] is not implemented'.format(pad_type))
-    return layer
+    return None
 
 
 def get_valid_padding(kernel_size, dilation):
@@ -66,11 +36,7 @@ class ConcatBlock(nn.Module):
         output = torch.cat((x, self.sub(x)), dim=1)
         return output
 
-    def __repr__(self):
-        tmpstr = 'Identity .. \n|'
-        modstr = self.sub.__repr__().replace('\n', '\n|')
-        tmpstr = tmpstr + modstr
-        return tmpstr
+
 
 
 class ShortcutBlock(nn.Module):
@@ -83,19 +49,8 @@ class ShortcutBlock(nn.Module):
         output = x + self.sub(x)
         return output
 
-    def __repr__(self):
-        tmpstr = 'Identity + \n|'
-        modstr = self.sub.__repr__().replace('\n', '\n|')
-        tmpstr = tmpstr + modstr
-        return tmpstr
-
 
 def sequential(*args):
-    # Flatten Sequential. It unwraps nn.Sequential.
-    if len(args) == 1:
-        if isinstance(args[0], OrderedDict):
-            raise NotImplementedError('sequential does not support OrderedDict input.')
-        return args[0]  # No sequential is needed.
     modules = []
     for module in args:
         if isinstance(module, nn.Sequential):
@@ -108,12 +63,6 @@ def sequential(*args):
 
 def conv_block(in_nc, out_nc, kernel_size, stride=1, dilation=1, groups=1, bias=True, \
                pad_type='zero', norm_type=None, act_type='relu', mode='CNA'):
-    '''
-    Conv layer with padding, normalization, activation
-    mode: CNA --> Conv -> Norm -> Act
-        NAC --> Norm -> Act --> Conv (Identity Mappings in Deep Residual Networks, ECCV16)
-    '''
-    assert mode in ['CNA', 'NAC', 'CNAC'], 'Wong conv mode [{:s}]'.format(mode)
     padding = get_valid_padding(kernel_size, dilation)
     p = pad(pad_type, padding) if pad_type and pad_type != 'zero' else None
     padding = padding if pad_type == 'zero' else 0
@@ -121,18 +70,8 @@ def conv_block(in_nc, out_nc, kernel_size, stride=1, dilation=1, groups=1, bias=
     c = nn.Conv2d(in_nc, out_nc, kernel_size=kernel_size, stride=stride, padding=padding, \
             dilation=dilation, bias=bias, groups=groups)
     a = act(act_type) if act_type else None
-    if 'CNA' in mode:
-        n = norm(norm_type, out_nc) if norm_type else None
-        return sequential(p, c, n, a)
-    elif mode == 'NAC':
-        if norm_type is None and act_type is not None:
-            a = act(act_type, inplace=False)
-            # Important!
-            # input----ReLU(inplace)----Conv--+----output
-            #        |________________________|
-            # inplace ReLU will modify the input, therefore wrong output
-        n = norm(norm_type, in_nc) if norm_type else None
-        return sequential(n, a, p, c)
+    n = norm(norm_type, out_nc) if norm_type else None
+    return sequential(p, c, n, a)
 
 
 ####################
--- models/networks.py
+++ models/networks.py
@@ -4,67 +4,24 @@ import torch.nn as nn
 from torch.nn import init
 import models.modules.architecture as arch
 
-def weights_init_normal(m, std=0.02):
-    classname = m.__class__.__name__
-    if classname.find('Conv') != -1:
-        init.normal_(m.weight.data, 0.0, std)
-        if m.bias is not None:
-            m.bias.data.zero_()
-    elif classname.find('Linear') != -1:
-        init.normal_(m.weight.data, 0.0, std)
-        if m.bias is not None:
-            m.bias.data.zero_()
-    elif classname.find('BatchNorm2d') != -1:
-        init.normal_(m.weight.data, 1.0, std)  # BN also uses norm
-        init.constant_(m.bias.data, 0.0)
 
-
-def weights_init_kaiming(m, scale=1):
-    classname = m.__class__.__name__
-    if classname.find('Conv') != -1:
-        init.kaiming_normal_(m.weight.data, a=0, mode='fan_in')
-        m.weight.data *= scale
-        if m.bias is not None:
-            m.bias.data.zero_()
-    elif classname.find('Linear') != -1:
-        init.kaiming_normal_(m.weight.data, a=0, mode='fan_in')
-        m.weight.data *= scale
-        if m.bias is not None:
-            m.bias.data.zero_()
-    elif classname.find('BatchNorm2d') != -1:
-        init.constant_(m.weight.data, 1.0)
-        init.constant_(m.bias.data, 0.0)
-
-
-def weights_init_orthogonal(m):
-    classname = m.__class__.__name__
-    if classname.find('Conv') != -1:
-        init.orthogonal_(m.weight.data, gain=1)
-        if m.bias is not None:
-            m.bias.data.zero_()
-    elif classname.find('Linear') != -1:
-        init.orthogonal_(m.weight.data, gain=1)
-        if m.bias is not None:
-            m.bias.data.zero_()
-    elif classname.find('BatchNorm2d') != -1:
-        init.constant_(m.weight.data, 1.0)
-        init.constant_(m.bias.data, 0.0)
-
-
-def init_weights(net, init_type='kaiming', scale=1, std=0.02):
-    # scale for 'kaiming', std for 'normal'.
-    print('Initialization method [{:s}]'.format(init_type))
-    if init_type == 'normal':
-        weights_init_normal_ = functools.partial(weights_init_normal, std=std)
-        net.apply(weights_init_normal_)
-    elif init_type == 'kaiming':
-        weights_init_kaiming_ = functools.partial(weights_init_kaiming, scale=scale)
-        net.apply(weights_init_kaiming_)
-    elif init_type == 'orthogonal':
-        net.apply(weights_init_orthogonal)
-    else:
-        raise NotImplementedError('initialization method [{:s}] not implemented'.format(init_type))
-
+def weights_by(scale=1):
+    def helper(m):
+        classname = m.__class__.__name__
+        if classname.find('Conv') != -1:
+            init.kaiming_normal_(m.weight.data, a=0, mode='fan_in')
+            m.weight.data *= scale
+            if m.bias is not None:
+                m.bias.data.zero_()
+        elif classname.find('Linear') != -1:
+            init.kaiming_normal_(m.weight.data, a=0, mode='fan_in')
+            m.weight.data *= scale
+            if m.bias is not None:
+                m.bias.data.zero_()
+        elif classname.find('BatchNorm2d') != -1:
+            init.constant_(m.weight.data, 1.0)
+            init.constant_(m.bias.data, 0.0)
+    return helper
 
 
 ####################
@@ -81,7 +38,8 @@ def define_G(opt):
             act_type='leakyrelu', mode=opt_net['mode'], upsample_mode='upconv')
 
     if opt['is_train']:
-        init_weights(netG, init_type='kaiming', scale=0.1)
+        netG.apply(weights_by(0.1))
+
     if gpu_ids:
         assert torch.cuda.is_available()
         netG = nn.DataParallel(netG)
@@ -96,7 +54,7 @@ def define_D(opt):
     netD = arch.Discriminator_VGG_128(in_nc=opt_net['in_nc'], base_nf=opt_net['nf'], \
             norm_type=opt_net['norm_type'], mode=opt_net['mode'], act_type=opt_net['act_type'])
 
-    init_weights(netD, init_type='kaiming', scale=1)
+    netD.apply(weights_by(1))
     if gpu_ids:
         netD = nn.DataParallel(netD)
     return netD
@@ -105,14 +63,12 @@ def define_D(opt):
 def define_F(opt, use_bn=False):
     gpu_ids = opt['gpu_ids']
     device = torch.device('cuda' if gpu_ids else 'cpu')
-    # pytorch pretrained VGG19-54, before ReLU.
     if use_bn:
         feature_layer = 49
     else:
         feature_layer = 34
     netF = arch.VGGFeatureExtractor(feature_layer=feature_layer, use_bn=use_bn, \
         use_input_norm=True, device=device)
-    # netF = arch.ResNet101FeatureExtractor(use_input_norm=True, device=device)
     if gpu_ids:
         netF = nn.DataParallel(netF)
     netF.eval()  # No need to train
