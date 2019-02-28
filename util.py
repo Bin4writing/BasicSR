155d510f43da4feb22ad4f51272dcfb4a8c2cc2e
Bin4writing <is.zhang.yueyou@gmail.com>
2019-02-28 16:40:28 +0800

    add get_timestamp

--- util.py
+++ util.py
@@ -12,6 +12,8 @@ import logging
 # miscellaneous
 ####################
 
+def get_timestamp():
+    return datetime.now().strftime('%y%m%d-%H%M%S')
 
 
 
@@ -77,8 +79,7 @@ def mkdir_and_rename(path):
     if os.path.exists(path):
         new_name = path + '_archived_' + get_timestamp()
         print('Path already exists. Rename it to [{:s}]'.format(new_name))
-        logger = logging.getLogger('base')
-        logger.info('Path already exists. Rename it to [{:s}]'.format(new_name))
+        print('Path already exists. Rename it to [{:s}]'.format(new_name))
         os.rename(path, new_name)
     os.makedirs(path)
 
